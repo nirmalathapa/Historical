c1fc6c5fc8e5ea60ddd6616d9b9feb9df000b197
Rajesh Sharma <rajesh@movielala.com>
2017-04-29 23:20:24 +0200

    Add gentelella theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root to: 'measurements#new'
+
   resources :measurements, only: [:new, :create]
   resource :measurements, only: :show
 end
