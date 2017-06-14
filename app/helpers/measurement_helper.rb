4381a7363d8c1f0118c8459f21ae870febda6db0
Rajesh Sharma <rajesh@surf.net.np>
2017-06-14 20:33:53 +0200

    lower bodyfat is better

--- app/helpers/measurement_helper.rb
+++ app/helpers/measurement_helper.rb
@@ -1,6 +1,6 @@
 module MeasurementHelper
   def css_for_change(type, change)
-    if type == "waist"
+    if %q(waist bodyfat).include? type
       change.positive? ? "worse" : "better"
     else
       change.positive? ? "better" : "worse"
