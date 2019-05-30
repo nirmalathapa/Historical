eaf083e520c29da775509d63db2a2c084e4762dc
Rajesh Sharma <rajesh.sharma@karnovgroup.com>
2019-05-30 13:37:41 +0200

    Upgrade webpacker

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,6 +1,5 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
 const environment = require('./environment')
 
-const merge = require('webpack-merge')
-const customConfig = require('./shared')
-
-module.exports = merge(environment.toWebpackConfig(), customConfig)
+module.exports = environment.toWebpackConfig()
--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,5 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+
 const environment = require('./environment')
 
-const merge = require('webpack-merge')
-const customConfig = require('./shared')
-
-module.exports = merge(environment.toWebpackConfig(), customConfig)
+module.exports = environment.toWebpackConfig()
